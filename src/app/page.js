ad70834ec5fe2a19c71d1d6644d8b5a0d1a1b6ee
ironhamlab <03110315@naver.com>
2026-04-27 04:13:43 +0900

    feat/홈페이지

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+'use client'
+import PromptList from "@/components/home/PromptList"
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>프롬프트 저장소</h1>
+      <PromptList />
+    </main>
+  )
+}
