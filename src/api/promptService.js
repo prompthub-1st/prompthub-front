3c833f8370128075b3fb4531004994867351dad0
ironhamlab <03110315@naver.com>
2026-04-27 15:31:31 +0900

    feat: 내용 수정 및 삭제 기능 추가

--- src/api/promptService.js
+++ src/api/promptService.js
@@ -20,9 +20,39 @@ export const fetchAllData = async () => {
 };
 
 
-// 특정 아이디의 상세 정보를 가져오는 서비스
-export const fetchPromptById = async (id) => {};
-
+// 특정 아이디의 상세 정보 1개를 가져오는 서비스
+export const fetchPromptById = async (id) => {
+  const res = await fetch(`${BASE_URL}/prompts/${id}?_expand=user&_expand=category`);
+  if (!res.ok) throw new Error("데이터를 불러오지 못했습니다.");
+  return res.json();
+};
 
 // 새 프롬프트를 등록하는 서비스
-export const createPrompt = async (data) => {};
+export const createPrompt = async (data) => {
+  const res = await fetch(`${BASE_URL}/prompts`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error('네트워크 응답에 문제가 있습니다.');
+
+  return res.json();
+};
+
+// 수정하기 (PUT)
+export const updatePrompt = async ({ id, ...updateData }) => {
+  const res = await fetch(`${BASE_URL}/prompts/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updateData),
+  });
+  return res.json();
+};
+
+// 삭제하기 (DELETE)
+export const deletePrompt = async (id) => {
+  await fetch(`${BASE_URL}/prompts/${id}`, {
+    method: 'DELETE',
+  });
+};
